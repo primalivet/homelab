89610d7077e5f0a0438173275dff64ee5cf0e96a
Gustaf Holm <gustafholm1@gmail.com>
2025-05-30 11:43:47 +0200

    switch secrets format and rename k3s token (require cluster reset)

--- modules/homelab-secrets.nix
+++ modules/homelab-secrets.nix
@@ -1,8 +1,9 @@
 { config, pkgs, sops, ... }:
 {
   sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-  sops.defaultSopsFile = ../secrets.yaml;
-  sops.secrets.k3s_token = {
+  sops.defaultSopsFile = ../.secret.env;
+  sops.defaultSopsFormat = "dotenv";
+  sops.secrets.K3S_TOKEN = {
     owner = "root";
     group = "root";
     mode = "0400";
